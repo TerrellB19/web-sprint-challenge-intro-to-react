2a600414858f60276d82dfadccb68310978a9bf7
Terrell Booker <TerrellBooker19@gmail.com>
2023-02-27 15:16:26 -0500

    fixed error?

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,18 @@
+import React from "react";
+
 // Write your Character component here
+
+const Character = ({props}) => {
+
+
+    return (
+        <div>
+            <h2>            {props.name}</h2>
+            <p>Gender:      {props.gender}</p>
+            <p>Height:      {props.height}</p>
+            <p>Birth Year:  {props.birth_year}</p>
+        </div>
+    )
+}
+
+export default Character
